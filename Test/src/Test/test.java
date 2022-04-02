7167f7d954798838b7cf088bb8d0cd14b77d6de4
Miroslav Babeľa <miroslav.babela.jr@gmail.com>
2022-04-02 14:45:20 +0200

    Delete Test/src/Test directory

--- Test/src/Test/test.java
+++
@@ -1,9 +0,0 @@
-package Test;
-public class test {
-
-    public static void main(String[] args) {
-        Databaze db = new Databaze(5);
-        db.setMeno("Miro", 0);
-        System.out.println(db.getMeno(0));
-    }
-}
